c9626eb308575e627842634a43cee26210cb761e
Ukhti Salsabila <ukhtisalsabilaa@gmail.com>
2020-04-27 23:32:39 +0700

    splash-menut

---
+++ app/src/main/java/com/example/laundryking/MenuUtamaActivity.java
@@ -0,0 +1,14 @@
+package com.example.laundryking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuUtamaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_utama);
+    }
+}
